1baf93ed2fbfc1c97c1e6b28923a7f6ec2f52194
Andriamanitra <mikko77792@gmail.com>
2023-05-15 08:10:01 +0300

    add leetcode provider (closes #2)

---
+++ src/providers/leetcode.cr
@@ -0,0 +1,49 @@
+class LeetcodeStats < SiteStats
+  @@url_regex = Regex.new("https?://(www[.])?leetcode[.]com/(?<username>\\w+)")
+  @@leetcode_api_url = "https://leetcode.com/graphql/"
+
+  @@headers = HTTP::Headers{
+    "Content-Type" => "application/json",
+    "Referer"      => "https://leetcode.com/Andriamanitra/",
+  }
+
+  def self.fetch_lang_stats(username : String) : Hash(Langname, Int32)?
+    stats = Hash(Langname, Int32).new(0)
+
+    body = {
+      "query": <<-GRAPHQL,
+        query languageStats($username: String!) {
+            matchedUser(username: $username) {
+                languageProblemCount {
+                    languageName
+                    problemsSolved
+                }
+            }
+        }
+      GRAPHQL
+      "variables":     {"username": username},
+      "operationName": "languageStats",
+    }.to_json
+    response = HTTP::Client.post(@@leetcode_api_url, @@headers, body)
+    return nil unless response.success?
+
+    obj = JSON.parse(response.body).dig?("data", "matchedUser", "languageProblemCount")
+    return nil if obj.nil?
+
+    obj.as_a.each do |lang|
+      lang_name = lang["languageName"].as_s
+      solved_count = lang["problemsSolved"].as_i
+      stats[lang_name] += solved_count
+    end
+
+    stats
+  end
+
+  def self.from_profile_url(profile_url : String) : SiteStats?
+    if username = @@url_regex.match(profile_url).try(&.["username"])
+      stats = self.fetch_lang_stats(username)
+      return nil if stats.nil?
+      self.new(username, profile_url, stats)
+    end
+  end
+end
