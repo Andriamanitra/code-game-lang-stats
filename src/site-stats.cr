e2a6abac95b3d5b974cd47378d5b194d369e983b
Andriamanitra <mikko77792@gmail.com>
2023-05-15 03:59:46 +0300

    start adding language aliases

--- src/site-stats.cr
+++ src/site-stats.cr
@@ -3,10 +3,61 @@ class SiteStats
 
   alias Langname = String
 
+  @@lang_aliases = {
+    "awk"        => "AWK",
+    "bash"       => "Bash",
+    "c"          => "C",
+    "c++"        => "C++",
+    "cpp"        => "C++",
+    "csharp"     => "C#",
+    "c#"         => "C#",
+    "clojure"    => "Clojure",
+    "commonlisp" => "Lisp",
+    "crystal"    => "Crystal",
+    "d"          => "D",
+    "elixir"     => "Elixir",
+    "fortran"    => "Fortran",
+    "fsharp"     => "F#",
+    "f#"         => "F#",
+    "golang"     => "Go",
+    "go"         => "Go",
+    "haskell"    => "Haskell",
+    "julia"      => "Julia",
+    "janet"      => "Janet",
+    "java"       => "Java",
+    "javascript" => "JS",
+    "js"         => "JS",
+    "kotlin"     => "Kotlin",
+    "lisp"       => "Lisp",
+    "lua"        => "Lua",
+    "nim"        => "Nim",
+    "ocaml"      => "OCaml",
+    "pascal"     => "Pascal",
+    "perl"       => "Perl",
+    "powershell" => "PS",
+    "python"     => "Python",
+    "python2"    => "Python",
+    "python3"    => "Python",
+    "raku"       => "Raku",
+    "ruby"       => "Ruby",
+    "rust"       => "Rust",
+    "scala"      => "Scala",
+    "scheme"     => "Scheme",
+    "sql"        => "SQL",
+    "tcl"        => "Tcl",
+    "typescript" => "TS",
+    "ts"         => "TS",
+    "wren"       => "Wren",
+    "zig"        => "Zig",
+  }
+
   def initialize(
     @username : String,
     @profile_url : String,
-    @solved_by_language : Hash(Langname, Int32),
+    @solved_by_language : Hash(Langname, Int32)
   )
+    @solved_by_language = @solved_by_language.transform_keys do |lang|
+      @@lang_aliases.fetch(lang.downcase.gsub(/[\s-]/, ""), lang)
+    end
   end
 end
